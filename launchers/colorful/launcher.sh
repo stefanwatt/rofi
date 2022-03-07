527b4941dd7faa668fd7bb36e423253b27434020
stefan watt <stefan.watt@gmail.com>
2022-03-08 00:54:45 +0100

    initial commit

---
+++ launchers/colorful/launcher.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+# Available Styles
+# >> Created and tested on : rofi 1.6.0-1
+#
+# style_1     style_2     style_3     style_4     style_5     style_6
+# style_7     style_8     style_9     style_10    style_11    style_12
+
+theme="style_1"
+dir="$HOME/.config/rofi/launchers/colorful"
+
+# dark
+ALPHA="#2E344000"
+BG="#2E3440ff"
+FG="#D8DEE9ff"
+SELECT="#4C566Aff"
+
+# light
+#ALPHA="#2E344000"
+#BG="#D8DEE9ff"
+#FG="#2E3440ff"
+#SELECT="#f3f3f3ff"
+
+ACCENT="#8FBCBBff"
+
+# overwrite colors file
+cat > $dir/colors.rasi <<- EOF
+	/* colors */
+
+	* {
+	  al:  $ALPHA;
+	  bg:  $BG;
+	  se:  $SELECT;
+	  fg:  $FG;
+	  ac:  $ACCENT;
+	}
+EOF
+
+# comment these lines to disable random style
+# themes=($(ls -p --hide="launcher.sh" --hide="colors.rasi" $dir))
+# theme="${themes[$(( $RANDOM % 12 ))]}"
+
+rofi -no-lazy-grab -show drun -modi drun -theme $dir/"$theme"
